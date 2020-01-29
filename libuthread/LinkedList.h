093f95184996ac2f25670e2814d9955f3674a5a8
Justin Lim <juylim@ucdavis.edu>
2020-01-28 17:39:08 -0800

    completed create, enqueue, dequeue, delete

---
+++ libuthread/LinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by Justin Lim on 1/28/20.
+//
+
+#ifndef LIBUTHREAD_LINKEDLIST_H
+#define LIBUTHREAD_LINKEDLIST_H
+
+struct ListNode {
+    void* item;
+    struct ListNode * next;
+};
+
+
+#endif //LIBUTHREAD_LINKEDLIST_H
--- libuthread/queue.c
+++ libuthread/queue.c
@@ -3,14 +3,22 @@
 #include <string.h>
 
 #include "queue.h"
+#include "LinkedList.h"
 
 struct queue {
-	/* TODO Phase 1 */
+    struct LinkedList* head;
+    int length;
+    struct LinkedList* curr;
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t myQueue;
+    myQueue->head = (struct ListNode *) malloc(sizeof(struct ListNode));
+    myQueue->head->next = NULL;
+    myQueue->head->item = NULL;
+    myQueue->curr = head;
+	myQueue->length = 0;
 }
 
 int queue_destroy(queue_t queue)
@@ -20,17 +28,60 @@ int queue_destroy(queue_t queue)
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+    struct ListNode* nextN = (struct ListNode *) malloc(sizeof(struct ListNode));
+    nextN->next = NULL;
+    nextN->item = data;
+    if (nextN == NULL || data == NULL) {
+        return -1;
+    }
+	queue->curr->next = nextN;
+	queue->curr = queue->curr->next;
+	queue->length++;
+	return 0;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+    struct ListNode* tempHead = queue->head->next;
+    // If data or queue is empty, return -1
+    if ((*data) == NULL || queue->length == 0 || tempHead == NULL){
+        return -1;
+    }
+
+    // Assign the oldest item in the queue to the data pointer
+    (*data) = queue->head->item;
+    free(queue->head);
+    queue->head = tempHead;
+	queue->length--;
+
+	return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+    struct ListNode* curr = queue->head;
+    struct ListNode* prev = NULL;
+	if ((*data) == NULL || queue->head == NULL) {
+	     return -1;
+	}
+
+	while(curr->next != NULL) {
+	    if ((*data) == (*curr->item)) {
+            if (prev == NULL) {
+                queue_dequeue(queue, NULL);
+            }
+            else {
+                prev->next = curr->next;
+                free(curr);
+                queue->length--;
+            }
+            return 0;
+	    }
+	    prev = curr;
+	    curr = curr->next;
+	}
+
+	return -1;
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
