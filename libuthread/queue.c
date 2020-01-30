834e74455aefe6c856a1047abe69745f5a798db3
Justin Lim <juylim@ucdavis.edu>
2020-01-29 17:19:20 -0800

    Fixed some errors

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -2,29 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <stdio.h>
+
+#include <ctype.h>
+
+
 #include "queue.h"
 #include "LinkedList.h"
 
 struct queue {
-    struct LinkedList* head;
+    struct ListNode* head;
     int length;
-    struct LinkedList* curr;
+    struct ListNode* curr;
 };
 
 queue_t queue_create(void)
 {
-	queue_t myQueue;
+	queue_t myQueue = (queue_t) malloc(sizeof(struct queue));
     myQueue->head = (struct ListNode *) malloc(sizeof(struct ListNode));
     myQueue->head->next = NULL;
     myQueue->head->item = NULL;
-    myQueue->curr = head;
+    myQueue->curr = myQueue->head;
 	myQueue->length = 0;
+	return myQueue;
 }
 
-int queue_destroy(queue_t queue)
+/*int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
-}
+	*//* TODO Phase 1 *//*
+}*/
 
 int queue_enqueue(queue_t queue, void *data)
 {
@@ -44,7 +50,7 @@ int queue_dequeue(queue_t queue, void **data)
 {
     struct ListNode* tempHead = queue->head->next;
     // If data or queue is empty, return -1
-    if ((*data) == NULL || queue->length == 0 || tempHead == NULL){
+    if ((data) == NULL || queue->head == NULL || tempHead == NULL){
         return -1;
     }
 
@@ -61,12 +67,12 @@ int queue_delete(queue_t queue, void *data)
 {
     struct ListNode* curr = queue->head;
     struct ListNode* prev = NULL;
-	if ((*data) == NULL || queue->head == NULL) {
+	if ((data) == NULL || queue->head == NULL) {
 	     return -1;
 	}
 
 	while(curr->next != NULL) {
-	    if ((*data) == (*curr->item)) {
+	    if ((data) == ((curr->item))) {
             if (prev == NULL) {
                 queue_dequeue(queue, NULL);
             }
@@ -86,11 +92,23 @@ int queue_delete(queue_t queue, void *data)
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 {
-	/* TODO Phase 1 */
+	 /* TODO Phase 1 */
 }
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+	 /* TODO Phase 1 */
 }
+
+int main() {
+    queue_t q;
+    int data = 3, *ptr;
+
+    q = queue_create();
+    printf("%d\n",queue_enqueue(q, &data));
+    printf("%d\n",queue_dequeue(q, (void**)&ptr));
+
+
+    return 0;
+};
 
