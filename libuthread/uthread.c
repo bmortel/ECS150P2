f05f15c2531eb20feab088dba11259416ad753e1
Justin Lim <juylim@ucdavis.edu>
2020-02-03 18:27:56 -0800

    Fixed memory issues

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -20,7 +20,7 @@ queue_t zombieQueue;
 queue_t blockedQueue;
 int currTID = 0;
 struct Tcb* currTcb;
-bool init = 0;
+bool init = false;
 
 void uthread_yield(void)
 {
@@ -43,17 +43,20 @@ int uthread_create(uthread_func_t func, void *arg)
     if (!init) {
         uthread_init(func, arg);
     }
-
-    struct Tcb *tcb = malloc(sizeof(struct Tcb));
-    tcb->ctx = malloc(sizeof(uthread_ctx_t));
-    tcb->stack = uthread_ctx_alloc_stack();
-    if (uthread_ctx_init(*(&tcb->ctx), tcb->stack, func, arg) == -1) {
+    printf("hi\n");
+    struct Tcb* tb;
+    tb = malloc(sizeof(struct Tcb));
+    printf("hi\n");
+    tb->ctx = malloc(sizeof(uthread_ctx_t));
+    printf("hi\n");
+    tb->stack = uthread_ctx_alloc_stack();
+   /* if (uthread_ctx_init(tb->ctx, tb->stack, func, arg) == -1) {
         return -1;
-    }
+    }*/
     TIDCount++;
-    tcb->tid = TIDCount;
-    tcb->state = "ready";
-    queue_enqueue(readyQueue, tcb);
+    tb->tid = TIDCount;
+    tb->state = "ready";
+    //queue_enqueue(readyQueue, tb);
     return TIDCount;
 }
 
@@ -77,13 +80,15 @@ int uthread_join(uthread_t tid, int *retval)
 
 }
 void uthread_init(uthread_func_t func, void *arg) {
+
     readyQueue = queue_create();
     zombieQueue = queue_create();
     blockedQueue = queue_create();
-    struct Tcb* main = malloc(sizeof(struct Tcb));
+    struct Tcb* main = (struct Tcb*)malloc(sizeof(struct Tcb));
     main->tid = 0;
     main->stack = uthread_ctx_alloc_stack();
-    uthread_ctx_init(*(&main->ctx), main->stack, func, arg);
+    main->ctx = malloc(sizeof(uthread_ctx_t));
+    uthread_ctx_init(main->ctx, main->stack, NULL, NULL);
     currTcb = main;
 }
 
