6cd72341fe6afaba84d4e7a316e067d2c55841e0
Brandon Mortel <bmmortel@gmail.com>
2020-02-04 16:14:14 -0800

    preempt_start done

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,6 +15,10 @@
  */
 #define HZ 100
 
+void signalHandler (int signum){
+	uthread_yield();
+}
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
@@ -28,5 +32,22 @@ void preempt_enable(void)
 void preempt_start(void)
 {
 	/* TODO Phase 4 */
+	struct sigaction sigStruct;
+	struct itimerval timer;
+	sigemptyset(&sigStruct.sa_mask);
+	sigStruct.sa_flags = 0;
+	sigStruct.sa_handler = signalHandler;
+	if (sigaction(SIGVTALRM, &sigStruct, NULL) == -1){
+		perror("sigaction");
+		exit(EXIT_FAILURE);
+	}
+	timer.it_value.tv_sec = 1/HZ;
+	timer.it_interval = timer.it_value;
+	if (setitimer(ITIMER_VIRTUAL, &timer, NULL) == -1){
+		perror("timer");
+		exit(EXIT_FAILURE);
+	}
+
+	
 }
 
