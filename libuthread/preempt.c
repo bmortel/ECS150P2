40c78d38cbc4830025139b756a86c3dea1918083
Brandon Mortel <bmmortel@gmail.com>
2020-02-04 16:27:13 -0800

    preempt.c done

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -1,3 +1,5 @@
+#include "preempt.h"
+
 #include <signal.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -6,7 +8,6 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-#include "preempt.h"
 #include "uthread.h"
 
 /*
@@ -15,39 +16,39 @@
  */
 #define HZ 100
 
-void signalHandler (int signum){
-	uthread_yield();
+sigset_t set;
+
+void signalHandler(int signum) { uthread_yield(); }
+
+void preempt_disable(void) {
+  /* TODO Phase 4 */
+  sigdelset(&set, SIGVTALRM);
 }
 
-void preempt_disable(void)
-{
-	/* TODO Phase 4 */
+void preempt_enable(void) { 
+	/* TODO Phase 4 */ 
+	sigaddset(&set, SIGVTALRM);
 }
 
-void preempt_enable(void)
-{
-	/* TODO Phase 4 */
+void preempt_start(void) {
+  /* TODO Phase 4 */
+  struct sigaction sigStruct;
+  struct itimerval timer;
+  sigemptyset(&sigStruct.sa_mask);
+  if (sigaddset(&set, SIGVTALRM)) {
+    perror("sigaddset error");
+    exit(EXIT_FAILURE);
+  }
+  sigStruct.sa_flags = 0;
+  sigStruct.sa_handler = signalHandler;
+  if (sigaction(SIGVTALRM, &sigStruct, NULL) == -1) {
+    perror("sigaction error");
+    exit(EXIT_FAILURE);
+  }
+  timer.it_value.tv_sec = 1 / HZ;
+  timer.it_interval = timer.it_value;
+  if (setitimer(ITIMER_VIRTUAL, &timer, NULL) == -1) {
+    perror("settimer error");
+    exit(EXIT_FAILURE);
+  }
 }
-
-void preempt_start(void)
-{
-	/* TODO Phase 4 */
-	struct sigaction sigStruct;
-	struct itimerval timer;
-	sigemptyset(&sigStruct.sa_mask);
-	sigStruct.sa_flags = 0;
-	sigStruct.sa_handler = signalHandler;
-	if (sigaction(SIGVTALRM, &sigStruct, NULL) == -1){
-		perror("sigaction");
-		exit(EXIT_FAILURE);
-	}
-	timer.it_value.tv_sec = 1/HZ;
-	timer.it_interval = timer.it_value;
-	if (setitimer(ITIMER_VIRTUAL, &timer, NULL) == -1){
-		perror("timer");
-		exit(EXIT_FAILURE);
-	}
-
-	
-}
-
